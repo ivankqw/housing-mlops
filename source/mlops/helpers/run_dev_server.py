a5aacd024c98be7eb828b16d34f7fb327f6b6ec6
Koh Quan Wei Ivan <ivankohquanwei@gmail.com>
2024-04-18 10:51:41 +0800

    update paths in monitoring and feedback

--- source/mlops/helpers/run_dev_server.py
+++ source/mlops/helpers/run_dev_server.py
@@ -9,8 +9,8 @@ from deploy import build_model_server_image, run_model_server_image
 if __name__ == "__main__":
     _ = load_dotenv("../../dev.env")
     model_uri = os.getenv("MODEL_URI")
-    image_name = os.getenv("IMAGE_NAME")
-    port = int(os.getenv("PORT"))
+    image_name = os.getenv("DEV_IMAGE_NAME")
+    port = int(os.getenv("DEV_PORT"))
 
     build_model_server_image(model_uri, image_name)
     run_model_server_image(image_name, port)
